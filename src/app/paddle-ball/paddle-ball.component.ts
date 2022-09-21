2f1fbacc5c047c63774bac547642cf91eaf7e40a
Janek Schoebel <j.schoebel@bitpioneers.com>
2022-09-21 22:53:56 +0200

    implemented basic paddle control

--- src/app/paddle-ball/paddle-ball.component.ts
+++ src/app/paddle-ball/paddle-ball.component.ts
@@ -9,22 +9,25 @@ import { Bodies, Body, Composite, Engine, Render, Runner, Constraint, MouseConst
 export class PaddleBallComponent implements OnInit {
   gameWidth: number = 1300;
   gameHeight: number = 700;
-  
+
+  paddle: Body = Bodies.rectangle(this.gameWidth/2, this.gameHeight-50, 200, 20, {inertia: Infinity, inverseInertia: 1/Infinity, isStatic: true});
+  ball: Body = Bodies.circle(this.gameWidth/2, 500, 10, {restitution: 1, frictionAir: 0, friction: 0, inertia: Infinity, inverseInertia: 1/Infinity});
+
   runner: Runner = Runner.create();
   
   //TODO: implement controls
-  // @HostListener("document:keypress", ["$event"])
-  // handleKeyboardEvent(event: KeyboardEvent) {
-  //   if (event.key === "a") {
-  //     Body.applyForce(this.paddle, Vector.create(-1, 0), Vector.create(-0.2, 0));
-  //   }
-  //   if (event.key === "d") {
-  //     Body.applyForce(this.paddle, Vector.create(1, 0), Vector.create(0.2, 0));
-  //   }
-  //   if (event.key === "w") {
-  //     Body.applyForce(this.ball, Vector.create(0, -1), Vector.create(0, -0.5));
-  //   }
-  // }
+  @HostListener("document:keypress", ["$event"])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    if (event.key === "a") {
+      Body.setPosition(this.paddle, Vector.create(this.paddle.position.x -100, this.paddle.position.y));
+    }
+    if (event.key === "d") {
+      Body.setPosition(this.paddle, Vector.create(this.paddle.position.x + 100, this.paddle.position.y));
+    }
+    if (event.key === "w") {
+      Body.applyForce(this.ball, Vector.create(0, -1), Vector.create(0, -0.01));
+    }
+  }
 
   constructor() {}
 
@@ -48,17 +51,14 @@ export class PaddleBallComponent implements OnInit {
 
     //create boundaries and push then to the components array
     const ceiling: Body = Bodies.rectangle(this.gameWidth/2, 0, this.gameWidth, 60, {isStatic: true});
-    const floor: Body = Bodies.rectangle(this.gameWidth/2, this.gameHeight, this.gameWidth, 60, {isStatic: true});
+    const floor: Body = Bodies.rectangle(this.gameWidth/2, this.gameHeight, this.gameWidth, 60, {isStatic: true, isSensor: true,});
     const leftWall: Body = Bodies.rectangle(0, this.gameHeight/2, 60, this.gameHeight, {isStatic: true});
     const rightWall: Body = Bodies.rectangle(this.gameWidth, this.gameHeight/2, 60, this.gameHeight, {isStatic: true});
 
     components.push(ceiling, floor, leftWall, rightWall);
 
-    //create other bodies and push them to the components array
-    const ball: Body = Bodies.circle(this.gameWidth/2, 500, 50, {restitution: 1, frictionAir: 0, friction: 0, inertia: Infinity, inverseInertia: 1/Infinity});
-    const paddle: Body = Bodies.rectangle(this.gameWidth/2, this.gameHeight-50, 300, 20, {inertia: Infinity, inverseInertia: 1/Infinity, restitution: 1});
-    
-    components.push(ball, paddle);
+    // push ball and paddle to the components array
+    components.push(this.ball, this.paddle);
 
     //create mouseConstraint and push it to the components array
     const mouseConstraint: MouseConstraint = MouseConstraint.create(engine, {mouse: Mouse.create(render.canvas)});
